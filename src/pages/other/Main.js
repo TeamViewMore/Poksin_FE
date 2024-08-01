7b2d087d7c4c8b68c1aad73a1be277198af4d933
khjhyunji <khjhyunji2003@naver.com>
2024-08-01 14:43:22 +0900

    💄 [Design] 가이드라인 스타일 변경
    [Design] 가이드라인 스타일 변경

--- src/pages/other/Main.js
+++ src/pages/other/Main.js
@@ -1,10 +1,70 @@
 import React from "react";
+import PlusBtn from "../../components/PlusBtn";
+import { useState } from "react";
+import Calendar from "react-calendar";
+import moment from "moment";
+import "react-calendar/dist/Calendar.css";
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 import * as M from "../../styles/other/MainStyle";
+import Group6 from "../../img/Group6.svg";
+
+import Group7 from "../../img/Group7.svg";
 
 function Main() {
+    const [value, onChange] = useState(new Date());
+
+    const navigate = useNavigate();
+
+    const handleDayRecordClick = (date) => {
+        navigate(`/calender/${moment(date).format("YYYY-MM-DD")}`);
+    };
+    const dates = [
+        moment(value).toDate(),
+        moment(value).add(1, "days").toDate(),
+        moment(value).add(2, "days").toDate(),
+        moment(value).add(3, "days").toDate(),
+    ];
     return (
         <>
-        <M.Main></M.Main>
+            <M.Main>
+                <M.Precious>
+                    <b>id 님,</b>
+                    <br />
+                    당신은 소중한 사람이에요. 누구도 당신을 해칠 수 없어요.
+                </M.Precious>
+                <M.CalenderList>
+                    <M.CalenderWrapper>
+                        <M.CalendarBox
+                            onChange={onChange}
+                            value={value}
+                            formatDay={(locale, date) => moment(date).format("D")}
+                            formatMonthYear={(locale, date) => moment(date).format("YYYY. MM.")}
+                            formatYear={(locale, date) => moment(date).format("YYYY")}
+                            calendarType="gregory"
+                            showNeighboringMonth={false}
+                            nextLabel=""
+                            prevLabel=""
+                            next2Label={null}
+                            prev2Label={null}
+                            minDetail="year"
+                        />
+                    </M.CalenderWrapper>
+                    <M.Happen>
+                        어떤 일이 있었나요?
+                        <M.UploadBox>
+                            <img src={Group6} alt="logo" style={{ width: "345px", height: "93px" }} />
+                        </M.UploadBox>
+                    </M.Happen>
+                    <M.Want>
+                        폭신폭신은 여러분을 돕고 싶습니다.
+                        <M.GuideBox>
+                            <img src={Group7} alt="logo" style={{ width: "345px", height: "93px" }} />
+                        </M.GuideBox>
+                    </M.Want>
+                    <PlusBtn></PlusBtn>
+                </M.CalenderList>
+            </M.Main>
         </>
     );
 }
--- src/styles/other/MainStyle.js
+++ src/styles/other/MainStyle.js
@@ -1,3 +1,147 @@
+import Calendar from "react-calendar";
 import styled from "styled-components";
+import "react-calendar/dist/Calendar.css";
+import CalendarPrev from "../../img/arrow_left.png";
+import CalendarNext from "../../img/arrow_right.png";
 
 export const Main = styled.div``;
+
+export const Happen = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const UploadBox = styled.div``;
+
+export const Precious = styled.div`
+    margin-top: 23px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 11pt;
+`;
+
+export const Want = styled.div`
+    margin-top: 54px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const GuideBox = styled.div``;
+
+export const CalenderList = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    margin: 0;
+`;
+
+export const CalenderWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #424242;
+    font-weight: 500;
+    font-size: 19px;
+    margin: 15px 0px;
+
+    .react-calendar {
+        border: none;
+        max-width: 330px;
+        width: 90%;
+    }
+
+    .react-calendar__navigation {
+        display: flex;
+        align-items: center;
+    }
+
+    .react-calendar__navigation__prev2-button,
+    .react-calendar__navigation__next2-button {
+        display: none;
+    }
+
+    .react-calendar__navigation__next-button {
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+        border: none;
+        cursor: pointer;
+        width: 8px;
+        height: 14px;
+        padding: 0;
+        margin: 0;
+        cursor: pointer;
+        background-image: url(${require("../../img/chevron.left.png")});
+    }
+
+    .react-calendar__navigation__label {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-weight: 550;
+        font-size: 19px;
+        pointer-events: none;
+    }
+
+    .react-calendar__navigation__label__labelText {
+        color: #424242;
+    }
+
+    .react-calendar__month-view__weekdays abbr {
+        text-decoration: none;
+    }
+
+    .react-calendar__month-view__weekdays {
+        margin-bottom: 10px;
+    }
+
+    .react-calendar__month-view__days__day--weekend {
+        color: #424242;
+    }
+
+    .react-calendar__tile abbr {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 35px;
+        height: 35px;
+        border-radius: 100px;
+    }
+
+    .react-calendar__tile--now {
+        background-color: #ded6ff;
+        color: #424242;
+        border-radius: 20px;
+    }
+
+    .react-calendar__month-view__days__day:focus,
+    .react-calendar__month-view__days__day:hover,
+    .react-calendar__tile--active {
+        background-color: #ded6ff;
+        color: #424242;
+        border-radius: 20px;
+    }
+`;
+
+export const CalendarBox = styled(Calendar)``;
+
+export const DayRecordBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 15px 30px;
+
+    .DayRecordBox-Date {
+        margin-bottom: 10px;
+    }
+
+    .DayRecordBox-Record {
+        font-weight: 300;
+        font-size: 15px;
+    }
+`;
