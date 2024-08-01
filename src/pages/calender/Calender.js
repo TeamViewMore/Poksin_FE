57e40347b779ab2280349ff01ae69b5a351bacc2
mogg22 <ahrthdus1@gmail.com>
2024-08-01 10:26:55 +0900

    💄 Fix : 캘린더 더보기 추가 수정
    탭 전환과 관련된 더보기 설정 추가 및 css 수정

--- src/pages/calender/Calender.js
+++ src/pages/calender/Calender.js
@@ -46,6 +46,12 @@ function Calender() {
         setShowMore(false);
     };
 
+    // 탭 변경 시 showMore 상태를 false로 변경
+    const handleTabClick = (index) => {
+        setTab(index);
+        setShowMore(false);
+    };
+
     // 이미지 상세보기
     const [selectedImg, setSelectedImg] = useState(null);
     const [selectedVideo, setSelectedVideo] = useState(null);
@@ -68,19 +74,19 @@ function Calender() {
                 <C.Header>{moment(value).format("YYYY. MM. DD.")}</C.Header>
                 <C.Contents>
                     <C.TabBar>
-                        <div className={`videoTab nameTab ${tab === 0 ? "selected" : ""}`} onClick={() => setTab(0)}>
+                        <div className={`videoTab nameTab ${tab === 0 ? "selected" : ""}`} onClick={() => handleTabClick(0)}>
                             동영상
                         </div>
-                        <div className={`recordTab nameTab ${tab === 1 ? "selected" : ""}`} onClick={() => setTab(1)}>
+                        <div className={`recordTab nameTab ${tab === 1 ? "selected" : ""}`} onClick={() => handleTabClick(1)}>
                             녹취
                         </div>
-                        <div className={`picTab nameTab ${tab === 2 ? "selected" : ""}`} onClick={() => setTab(2)}>
+                        <div className={`picTab nameTab ${tab === 2 ? "selected" : ""}`} onClick={() => handleTabClick(2)}>
                             사진
                         </div>
-                        <div className={`chatTab nameTab ${tab === 3 ? "selected" : ""}`} onClick={() => setTab(3)}>
+                        <div className={`chatTab nameTab ${tab === 3 ? "selected" : ""}`} onClick={() => handleTabClick(3)}>
                             상담
                         </div>
-                        <div className={`diaTab nameTab ${tab === 4 ? "selected" : ""}`} onClick={() => setTab(4)}>
+                        <div className={`diaTab nameTab ${tab === 4 ? "selected" : ""}`} onClick={() => handleTabClick(4)}>
                             진단서
                         </div>
                     </C.TabBar>
@@ -113,7 +119,12 @@ function Calender() {
                             <C.recordContent>
                                 <div className="top">
                                     <div className="title">폭언 녹음</div>
-                                    <button className="delete">삭제</button>
+                                    <C.More onClick={handleMoreClick}><img src={more} alt='더보기' /></C.More>
+                                    {showMore && (
+                                        <div ref={moreRef} style={{ position: 'absolute', top: 'calc(100% + 9px)', right: 0, zIndex: 1000 }}>
+                                            <More menu="기록 삭제" onMenuClick={handleMenuClick} />
+                                        </div>
+                                    )}
                                 </div>
                                 <div className="explan">가스라이팅 하는 말들</div>
                                 <div className="media">
@@ -128,7 +139,12 @@ function Calender() {
                             <C.picContent>
                                 <div className="top">
                                     <div className="title">폭행 cctv 영상</div>
-                                    <button className="delete">삭제</button>
+                                    <C.More onClick={handleMoreClick}><img src={more} alt='더보기' /></C.More>
+                                    {showMore && (
+                                        <div ref={moreRef} style={{ position: 'absolute', top: 'calc(100% + 9px)', right: 0, zIndex: 1000 }}>
+                                            <More menu="기록 삭제" onMenuClick={handleMenuClick} />
+                                        </div>
+                                    )}
                                 </div>
                                 <div className="explan">처음 맞은 거라 너무 무서웠고, 이때부터가 시작이었다...</div>
                                 <div className="media">
@@ -141,7 +157,12 @@ function Calender() {
                             <C.diaContent>
                                 <div className="top">
                                     <div className="title">진단서 1</div>
-                                    <button className="delete">삭제</button>
+                                    <C.More onClick={handleMoreClick}><img src={more} alt='더보기' /></C.More>
+                                    {showMore && (
+                                        <div ref={moreRef} style={{ position: 'absolute', top: 'calc(100% + 9px)', right: 0, zIndex: 1000 }}>
+                                            <More menu="기록 삭제" onMenuClick={handleMenuClick} />
+                                        </div>
+                                    )}
                                 </div>
                                 <div className="explan">저번 주에 싸우고 병원가서 떼온 진단서</div>
                                 <div className="media">
