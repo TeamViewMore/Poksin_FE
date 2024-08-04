7fd40bb898b4a38739a431ace10a20bdf6a33a22
mogg22 <ahrthdus1@gmail.com>
2024-08-04 21:11:12 +0900

    ✨ Feat : 프로필 수정 퍼블리싱
    ProfileUpdate.js 퍼블리싱 완료 api 연동 추후 예정

--- src/pages/chat/ProfileUpdate.js
+++ src/pages/chat/ProfileUpdate.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import * as P from "../../styles/chat/ProfileUpdateStyle";
 
+import profile from "../../img/profile_big.png";
+import Input from '../../components/Input';
+
 function ProfileUpdate() {
+    const [checkedStates, setCheckedStates] = useState({
+        phone: false,
+        emergency: false,
+        address: false,
+    });
+
+    const handleCheckboxChange = (type) => {
+        setCheckedStates((prevState) => ({
+            ...prevState,
+            [type]: !prevState[type],
+        }));
+    };
+
     return (
         <>
-            <P.ProfileUpdate></P.ProfileUpdate>
+            <P.Profile><img src={profile} alt='프로필' /></P.Profile>
+            <P.Box>
+                <P.Top>프로필 수정</P.Top>
+                <P.Middle>
+                    <Input 
+                        title="전화번호"
+                        isChecked={checkedStates.phone}
+                        onCheckboxChange={() => handleCheckboxChange('phone')}
+                    />
+                    <Input 
+                        title="긴급 연락처"
+                        isChecked={checkedStates.emergency}
+                        onCheckboxChange={() => handleCheckboxChange('emergency')}
+                    />
+                    <Input 
+                        title="주소"
+                        isChecked={checkedStates.address}
+                        onCheckboxChange={() => handleCheckboxChange('address')}
+                    />
+                </P.Middle>
+                <P.Bottom>
+                    <P.Button>수정 완료</P.Button>
+                </P.Bottom>
+            </P.Box>
         </>
     );
 }
--- src/styles/chat/ProfileUpdateStyle.js
+++ src/styles/chat/ProfileUpdateStyle.js
@@ -1,3 +1,65 @@
 import styled from "styled-components";
 
-export const ProfileUpdate = styled.div``;
+export const Profile = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 21px;
+    margin-bottom: 43px;
+    & img {
+        width: 202px;
+        height: 202px;
+    }
+`;
+
+export const Box = styled.div`
+    width: 100%;
+    height: 58vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    border-radius: 32px;
+    background: #FFF;
+    box-shadow: 0px -3px 3px 0px rgba(222, 214, 255, 0.25);
+`;
+
+export const Top = styled.div`
+    color: #000;
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 17px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+`;
+
+export const Middle = styled.div`
+    width: 320px;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+`;
+
+export const Bottom = styled.div`
+    width: 320px;
+    height: 51px;
+`;
+
+export const Button = styled.div`
+    width: 100%;
+    height: 100%;
+    border-radius: 20px;
+    background: var(--main, #7A29FF);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #FFF;
+    font-family: Pretendard;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+`;
