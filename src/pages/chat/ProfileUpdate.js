2e83ce434c5321b9e4ba17531e6f0ded76dc11ff
mogg22 <ahrthdus1@gmail.com>
2024-08-06 22:36:06 +0900

    🐛 Fix : 프로필 수정 input 이슈 해결
    프로필 수정 input 이슈 해결

--- src/pages/chat/ProfileUpdate.js
+++ src/pages/chat/ProfileUpdate.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import * as P from "../../styles/chat/ProfileUpdateStyle";
 import profile from "../../img/profile_big.png";
-import Input from '../../components/Input';
 import { useCookies } from 'react-cookie';
 import { useNavigate } from "react-router-dom";
 import { fetchUserData } from '../../utils/userApi';
+import checked from "../../img/check_checked_mini.png";
+import none from "../../img/check_none_mini.png";
 
 function ProfileUpdate() {
     const APIUrl = process.env.REACT_APP_API_URL;
@@ -28,13 +29,14 @@ function ProfileUpdate() {
         const fetchProfileData = async () => {
             try {
                 const token = cookies.accessToken;
+                // console.log(token);
                 if (!token) {
                     throw new Error("토큰을 찾을 수 없음");
                 }
                 const data = await fetchUserData(token);
-
-                if (data.code === "SUCCESS_RETRIEVE_USER") {
-                    const userData = data.data;
+                // console.log(data);
+                const userData = data.user;
+                    console.log(userData);
                     setFormValues({
                         phoneNum: userData.phoneNum || '',
                         emergencyNum: userData.emergencyNum || '',
@@ -45,7 +47,20 @@ function ProfileUpdate() {
                         emergency: userData.emergencyNum !== null,
                         address: userData.address !== null,
                     });
-                }
+                // if (data.code === "SUCCESS_RETRIEVE_USER") {
+                //     const userData = data.user;
+                //     console.log(userData);
+                //     setFormValues({
+                //         phoneNum: userData.phoneNum || '',
+                //         emergencyNum: userData.emergencyNum || '',
+                //         address: userData.address || '',
+                //     });
+                //     setCheckedStates({
+                //         phone: userData.phoneNum !== null,
+                //         emergency: userData.emergencyNum !== null,
+                //         address: userData.address !== null,
+                //     });
+                // }
             } catch (error) {
                 console.error("프로필 업데이트에서 프로필 가져오기 에러:", error);
             }
@@ -109,33 +124,46 @@ function ProfileUpdate() {
         }
     };
 
+    const setTitle = (title) => {
+        if (title === "phoneNum") {
+            return "전화번호 공개";
+        }
+        if (title === "emergencyNum") {
+            return "긴급 연락처 공개";
+        }
+        if (title === "address") {
+            return "주소 공개";
+        }
+        return title;
+    };
+
     return (
         <>
             <P.Profile><img src={profile} alt='프로필' /></P.Profile>
             <P.Box>
                 <P.Top>프로필 수정</P.Top>
                 <P.Middle>
-                    <Input 
-                        title="전화번호"
-                        isChecked={checkedStates.phone}
-                        onCheckboxChange={() => handleCheckboxChange('phone')}
-                        onChange={(e) => handleInputChange(e, 'phoneNum')}
-                        value={formValues.phoneNum}
-                    />
-                    <Input 
-                        title="긴급 연락처"
-                        isChecked={checkedStates.emergency}
-                        onCheckboxChange={() => handleCheckboxChange('emergency')}
-                        onChange={(e) => handleInputChange(e, 'emergencyNum')}
-                        value={formValues.emergencyNum}
-                    />
-                    <Input 
-                        title="주소"
-                        isChecked={checkedStates.address}
-                        onCheckboxChange={() => handleCheckboxChange('address')}
-                        onChange={(e) => handleInputChange(e, 'address')}
-                        value={formValues.address}
-                    />
+                    <P.InputBox>
+                        <P.Text><input type='text' placeholder='전화번호' value={formValues.phoneNum} onChange={(e) => handleInputChange(e, 'phoneNum')} /></P.Text>
+                        <P.Checkbox onClick={() => handleCheckboxChange('phone')}>
+                            <div className='title'>{setTitle('phoneNum')}</div>
+                            <div className='check'><img src={checkedStates.phone ? checked : none} alt='선택' /></div>
+                        </P.Checkbox>
+                    </P.InputBox>
+                    <P.InputBox>
+                        <P.Text><input type='text' placeholder='긴급 연락처' value={formValues.emergencyNum} onChange={(e) => handleInputChange(e, 'emergencyNum')} /></P.Text>
+                        <P.Checkbox onClick={() => handleCheckboxChange('emergency')}>
+                            <div className='title'>{setTitle('emergencyNum')}</div>
+                            <div className='check'><img src={checkedStates.emergency ? checked : none} alt='선택' /></div>
+                        </P.Checkbox>
+                    </P.InputBox>
+                    <P.InputBox>
+                        <P.Text><input type='text' placeholder='주소' value={formValues.address} onChange={(e) => handleInputChange(e, 'address')} /></P.Text>
+                        <P.Checkbox onClick={() => handleCheckboxChange('address')}>
+                            <div className='title'>{setTitle('address')}</div>
+                            <div className='check'><img src={checkedStates.address ? checked : none} alt='선택' /></div>
+                        </P.Checkbox>
+                    </P.InputBox>
                 </P.Middle>
                 <P.Bottom>
                     <P.Button onClick={handleSubmit}>수정 완료</P.Button>
